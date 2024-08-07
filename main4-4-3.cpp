63e509a1746fc17c57a9d63a00fc55620f733fef
satoshi-komatu <k023g1069@g.neec.ac.jp>
2024-08-07 16:08:23 +0900

    プロジェクト ファイルを追加します。

---
+++ main4-4-3.cpp
@@ -0,0 +1,59 @@
+﻿#include <stdio.h>
+
+class arithmetic
+{
+public:
+	virtual void Size() = 0;
+	virtual void Draw() = 0;
+
+protected:
+	float size;
+	float radius;
+};
+
+class circle :public arithmetic
+{
+public:
+	void Size();
+	void Draw() { printf("面積 %f\n", size); };
+};
+
+class square :public arithmetic
+{
+public:
+	void Size() override;
+	void Draw() { printf("面積 %f\n", size); };
+};
+
+void arithmetic::Size() {}
+
+void circle::Size()
+{
+	radius = 5.0f;
+	printf("円の半径 %f\n", radius);
+	size = radius * radius * 3.14f;
+}
+
+
+void square::Size()
+{
+	radius = 5.0f;
+	printf("矩形の半径 %f\n", radius);
+	size = radius * 2.0f * radius * 2.0f;
+}
+
+int main(void) {
+
+	arithmetic*ishape[2] = { new circle ,new square };
+
+	ishape[0]->Size();
+	ishape[1]->Size();
+	 
+	ishape[0]->Draw(); 
+	ishape[1]->Draw();
+
+	delete ishape[0]; 
+	delete ishape[1]; 
+
+	return 0;
+}
